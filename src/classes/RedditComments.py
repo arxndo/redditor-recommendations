53fba4ae0b1a9310b456b49626218faa6fbe2666
Ubuntu <ubuntu@ip-10-0-0-13.ec2.internal>
2020-01-23 22:20:17 +0000

    Connected Spark and S3

--- src/classes/RedditComments.py
+++ src/classes/RedditComments.py
@@ -30,11 +30,12 @@ class RedditComments:
         os.system("aws s3 mv RC_%d-%.2d s3://%s" \
             % (year, month, o.s3BucketName ) )
 
-    def clean(o, spark, month, year):
-        df = spark.read.json("RC_%d-%.2d" \
-                % (year, month) )
-        #df = spark.read.json("s3://%s/RC_%d-%.2d" \
-        #        % (o.s3BucketName, year, month) )
+    def clean(o, sparkS3, month, year):
+        spark = sparkS3.spark
+        #df = spark.read.json("RC_%d-%.2d" \
+                #% (year, month) )
+        df = spark.read.json("s3a://%s/RC_%d-%.2d" \
+                % (o.s3BucketName, year, month) )
         df.show()
 
     def show(o, month, year):
