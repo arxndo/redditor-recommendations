b7d44b059e06e9931ef5516286d79312d5b5bcbe
Ubuntu <ubuntu@ip-10-0-0-12.ec2.internal>
2020-02-03 23:26:55 +0000

    Node merging functionality added

--- src/scripts/mergeNodes.py
+++ src/scripts/mergeNodes.py
@@ -2,19 +2,17 @@ import os
 os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3.5'
 os.environ["PYSPARKDRIVER_PYTHON"]= "/usr/bin/python3.5"
 
-from CleanComments import CleanComments
 from MyContext import MyContext
-from Nodes import Nodes
+from MergedNodes import MergedNodes
 from Configuration import Configuration
 
 cfg = Configuration.configuration('config.yml')
 
-comments = Comments(cfg)
+context = MyContext().context(cfg, 'mergeNodes%s_%s' \
+                        % (cfg['dates']['startDate'], cfg['dates']['endDate']))
 
-context = MyContext().context(cfg)
+mergedNodes = MergedNodes(context, cfg)
 
-nodes = Nodes(context, comments, cfg)
-
-nodes.merge(cfg['dates']['startDate'], cfg['dates']['endDate'])
+mergedNodes.process(cfg['dates']['startDate'], cfg['dates']['endDate'])
 
 
