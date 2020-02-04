2471f3ae6cee25cc8e6a12d88f49be43ea51be54
Ubuntu <ubuntu@ip-10-0-0-12.ec2.internal>
2020-02-04 00:53:49 +0000

    Started tinkering with edge computation

--- src/classes/concrete/Edges.py
+++ src/classes/concrete/Edges.py
@@ -13,8 +13,9 @@ class Edges(GraphObject):
     def transform(self, date):
         self.df = self.df \
                       .groupBy("author") \
-                      .agg(F.collect_set("link_id" )) \
-                      .withColumnRenamed('collect_set(link_id)', 'link_ids')
+                      .agg(F.collect_set("link_id" ).alias('link_ids'))
+
+        self.df = self.df.repartition(200, F.col('link_ids'))
 
         df1 = self.df.withColumnRenamed('author', 'author_1') \
                      .withColumnRenamed('link_ids', 'link_ids_1')
@@ -26,16 +27,17 @@ class Edges(GraphObject):
                  .where(df1.author_1 < df2.author_2) \
                  .select('author_1', \
                        'author_2', \
-                       F.size(F.array_intersect('link_ids_1', 'link_ids_2'))) \
-                 .withColumnRenamed('size(array_intersect(link_ids_1, link_ids_2))', \
-                                    'weight') \
-                 .where('weight > %d' % self.truncation)
+                       F.size(F.array_intersect('link_ids_1', 'link_ids_2')) \
+                            .alias('weight'))
+
+        #self.df = self.df.repartition(200)
+        self.df = self.df.where('weight > %d' % self.truncation)
 
         return self
 
     def write(self, date):
-        n = self.partitions
-        self.df = self.df.repartition(n)
+        n = 1
+        self.df = self.df.repartition(n, F.col('weight'))
         self.df \
             .write \
             .parquet('s3a://%s/%s' \
