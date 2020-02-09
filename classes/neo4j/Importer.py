c0344e09d575027273b89d83264479408c27636d
Ubuntu <ubuntu@ip-10-0-0-4.ec2.internal>
2020-02-09 10:19:51 +0000

    Importing works, but still need more vCPU!

--- classes/neo4j/Importer.py
+++ classes/neo4j/Importer.py
@@ -8,19 +8,34 @@ class Importer:
         self.nodes1 = 'authors'
         self.nodes2 = 'subs'
         self.edges = 'author-sub'
+        self.path = '~/redditor-recommendations'
 
     def process(self, startDate, endDate):
         self.startDate = startDate
         self.endDate = endDate
-        if not os.path.isfile('tmp'):
-            os.system('mkdir tmp')
-        for name in [self.nodes1, self.nodes2]:
-            self.download(name)
+        self.downloadAll() \
+            .toDatabase()
+        #self.cleanUp()
+
+    def downloadAll(self):
+        if not os.path.isdir('%s/tmp' % self.path):
+            for name in [self.nodes1, self.nodes2, self.edges]:
+                self.download(name)
+        return self
 
     def download(self, name):
         dirName = '%s_%s_%s' % (name, self.startDate, self.endDate)
         options = '--recursive --exclude "*" --include "*.csv"'
         os.system('aws s3 cp s3://%s/%s/ tmp %s' \
                 % (self.bucket, dirName, options)) 
-        os.system('mv ~/redditor-recommendations/tmp/part* tmp/%s.csv' % name)
+        os.system('mv tmp/part*.csv tmp/%s.csv' %  name)
         #os.system('rm -rf tmp')
+
+    def cleanUp(self):
+        pass
+
+    def toDatabase(self):
+        os.system('sudo /usr/bin/neo4j stop')
+        os.system('sudo /usr/bin/neo4j-admin import --nodes:author="headers/authors_header.csv,tmp/authors.csv" --nodes:subreddit="headers/subs_header.csv,tmp/subs.csv" --relationships:post_to="headers/author-sub_header.csv,tmp/author-sub.csv" --ignore-duplicate-nodes true --ignore-missing-nodes true')
+        os.system('sudo /usr/bin/neo4j start')
+        return self
--- setup/addPythonPaths.sh
+++ setup/addPythonPaths.sh
@@ -1,6 +1,7 @@
 echo ''  >> ~/.bashrc
-echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/src/spark/batching' >> ~/.bashrc
-echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/src/spark/cleaning' >> ~/.bashrc
-echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/src/spark/graph' >> ~/.bashrc
-echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/src/settings' >> ~/.bashrc
-echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/src/analysis' >> ~/.bashrc
+echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/classes/spark/batching' >> ~/.bashrc
+echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/classes/spark/graph' >> ~/.bashrc
+echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/classes/spark/infrastructure' >> ~/.bashrc
+echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/classes/spark/merge' >> ~/.bashrc
+echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/classes/spark/cleaning' >> ~/.bashrc
+echo 'export PYTHONPATH=$PYTHONPATH:~/redditor-recommendations/scripts' >> ~/.bashrc
