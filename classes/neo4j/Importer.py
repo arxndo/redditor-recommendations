7a8ca5bc4d0ad651cd9644903c65f9f8d4216860
Ubuntu <ubuntu@ip-10-0-0-7.ec2.internal>
2020-02-09 06:36:06 +0000

    Succssful csv download, but need more local memory!

--- classes/neo4j/Importer.py
+++ classes/neo4j/Importer.py
@@ -12,18 +12,15 @@ class Importer:
     def process(self, startDate, endDate):
         self.startDate = startDate
         self.endDate = endDate
-        for name in [self.nodes1, self.nodes2, self.edges]:
+        if not os.path.isfile('tmp'):
+            os.system('mkdir tmp')
+        for name in [self.nodes1, self.nodes2]:
             self.download(name)
 
-    def download(name):
+    def download(self, name):
         dirName = '%s_%s_%s' % (name, self.startDate, self.endDate)
-        os.system('mkdir tmp')
-        os.system('aws s3 cp s3://%s/%s/*.csv tmp' dirName) 
+        options = '--recursive --exclude "*" --include "*.csv"'
+        os.system('aws s3 cp s3://%s/%s/ tmp %s' \
+                % (self.bucket, dirName, options)) 
+        os.system('mv ~/redditor-recommendations/tmp/part* tmp/%s.csv' % name)
         #os.system('rm -rf tmp')
-
-if __name__ == '_main_':
-        os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3.5'
-        os.environ["PYSPARKDRIVER_PYTHON"]= "/usr/bin/python3.5"]
-        cfg = Configuration.configuration('config.yml')
-        importer = Importer(cfg)
-        importer.process(cfg['dates']['startDate'], cfg['dates']['endDate'])
