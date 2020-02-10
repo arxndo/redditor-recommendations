62a1c0b7c3e9a5162bfe382108eed3cdc5043894
Ubuntu <ubuntu@ip-10-0-0-9.ec2.internal>
2020-02-10 17:03:32 +0000

    add neo4j installation instructions

---
+++ setup/neo4j-install.sh
@@ -0,0 +1,7 @@
+wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+
+echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+
+sudo apt update
+
+sudo apt install neo4j
