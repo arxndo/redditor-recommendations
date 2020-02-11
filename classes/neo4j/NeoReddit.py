d79df564603f0b19cbb980f294472714550b6340
Ubuntu <ubuntu@ip-10-0-0-9.ec2.internal>
2020-02-11 03:39:07 +0000

    Sub to Author and Author to Sub neo4j queries added to NeoReddit

--- classes/neo4j/NeoReddit.py
+++ classes/neo4j/NeoReddit.py
@@ -23,3 +23,15 @@ class NeoReddit:
                     topScore = record['s.score']
                     topSub = record['s.name']
             return topSub
+
+    def authorToSubs(self, name, n):
+        with self.driver.session() as session:
+            tx = session.begin_transaction()
+            result = tx.run("match (:author {name: {name}})-[r:post_to]->(s:subreddit) with s.name as sname, r.score as rscore order by rscore desc with collect([sname,rscore])[..{n}] as topsubs unwind topsubs as q return q[0] as subreddit, q[1] as score", name=name, n=n)  
+            return result
+
+    def subToAuthors(self, name, n):
+        with self.driver.session() as session:
+            tx = session.begin_transaction()
+            result = tx.run("match (a:author)-[r:post_to]->(s:subreddit {name: {name}}) with a.name as aname, r.score as rscore order by rscore desc with collect([aname,rscore])[..{n}] as topusers unwind topusers as q return q[0] as author, q[1] as score", name=name,n=n)
+            return result
--- neoTest.py
+++ neoTest.py
@@ -3,4 +3,13 @@ from NeoReddit import NeoReddit
 
 cfg = Configuration.configuration('config.yml')
 nr = NeoReddit(cfg)
-print(nr.topSub('arxndo'))
+
+print('')
+result = nr.authorToSubs('arxndo', 3)
+for item in result.records():
+    print(item['subreddit'])
+
+print('')
+result = nr.subToAuthors('batman', 3)
+for item in result.records():
+    print(item['author'])
