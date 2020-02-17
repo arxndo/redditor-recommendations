85bc8a10d735604ff703fe8b019f28afd3c683e1
Ubuntu <ubuntu@ip-10-0-0-9.ec2.internal>
2020-02-17 07:50:17 +0000

    added score percentages

--- flaskApp/forms.py
+++
@@ -1,7 +0,0 @@
-from wtforms import Form, StringField, SelectField
-
-class RedditorSearchForm(Form):
-    choices = [('Username', 'Username'), \
-                ('Subreddit', 'Subreddit')]
-    select = SelectField('', choices=choices)
-    search = StringField('')
--- flaskApp/views.py
+++ flaskApp/views.py
@@ -6,7 +6,6 @@ from NeoReddit import NeoReddit
 from Configuration import Configuration
 from flask import render_template, flash, request, redirect
 from flaskApp import app
-from forms import RedditorSearchForm
 
 @app.route('/about')
 def about():
@@ -22,56 +21,39 @@ def home():
 
     return render_template('home.html')
 
-
-@app.route('/results', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def search_results(name):
 
     cfg = Configuration.configuration('config.yml')
     nr = NeoReddit(cfg)
 
-    authors = nr.authorToAuthors(name, 3)
+    authorRecords = nr.authorToAuthors(name, 5)
 
-    if not authors:
+    if not authorRecords:
         return render_template('error.html' )
 
-    item1 = nr.authorToSubs(authors[0], 3)
-    subName1 = []
-    for sub in item1:
-        print(sub[0])
-        subName1.append(sub[0])
+    karma = nr.getKarma(name)
+    subList = nr.authorToSubs(name, 5)
+    selfSubs = []
+    for record in subList:
+        string = '%s %d%%' % (record[0], round(100*record[1]/karma))
+        selfSubs.append(string) 
+    
 
-    item2 = nr.authorToSubs(authors[1], 3)
-    subName2 = []
-    for sub in item2:
-        subName2.append(sub[0])
+    authors = []
+    subInfo = []
+    for record in authorRecords:
+        authorName = record[0]
+        authorKarma = nr.getKarma(authorName)
+        string = ''
+        authorSubs = nr.authorToSubs(authorName, 3)
+        for subRecord in authorSubs:
+            string += '%s %d%%, ' % (subRecord[0], round(100*subRecord[1]/authorKarma))
+        authors.append(authorName)
+        subInfo.append(string[:-2])
 
-    item3 = nr.authorToSubs(authors[2], 3)
-    subName3 = []
-    for sub in item3:
-        subName3.append(sub[0])
-
-    return render_template('results.html', name1=authors[0], \
-                                                name2=authors[1], \
-                                                name3=authors[2], \
-                                                sub11=subName1[0], \
-                                                sub12=subName1[1], \
-                                                sub13=subName1[2], \
-                                                sub21=subName2[0], \
-                                                sub22=subName2[1], \
-                                                sub23=subName2[2], \
-                                                sub31=subName3[0], \
-                                                sub32=subName3[1], \
-                                                sub33=subName3[2], \
-                                                )
-
-
-
-@app.route('/subs')
-def subs():
-    cfg = Configuration.configuration('config.yml')
-    nr = NeoReddit(cfg)
-    subs = nr.authorToSubs('kn0thing', 10)
-    string = ''
-    for sub in subs:
-        string += '\n%s' % sub[0]
-    return string
+    return render_template('results.html',  name = name, \
+                                            len=len(authors), \
+                                            subs = selfSubs, \
+                                            authors = authors, \
+                                            otherSubs = subInfo)
